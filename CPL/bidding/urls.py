ca7c57374ddfb167b3f8a640bbd02940fb87a25f
Nayeem2-2 <mursalinsarkar09@gmail.com>
2025-05-28 13:45:56 +0600

    added place bid button

--- CPL/bidding/urls.py
+++ CPL/bidding/urls.py
@@ -1,11 +1,8 @@
-# bidding/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_cards, name='show_cards'),  # Default page
-    #path('start/', views.start_bidding, name='start-bidding'),
-    #path('place/<int:player_id>/', views.place_bid, name='place-bid'),
-    #path('results/', views.bidding_results, name='bidding-results'),
+    path('', views.show_cards, name='show_cards'),
+    path('assign/<int:player_id>/', views.assign_team_and_increment_price, name='assign_player'),
+    path('api/player/<int:player_id>/', views.get_player_data, name='get_player_data'),
 ]
