f1e78ce19c5759a0bd0f0ac489f9573d1ed323b3
Nayeem2-2 <mursalinsarkar09@gmail.com>
2025-06-22 19:30:56 +0600

    increasing time bidding

--- CPL/bidding/models.py
+++ CPL/bidding/models.py
@@ -7,5 +7,12 @@ class AuctionSettings(models.Model):
     auction_start_time = models.DateTimeField(default=now)
     player_duration = models.DurationField(default=timedelta(seconds=20))
 
+    
+    player_default_duration = models.DurationField(default=timedelta(seconds=20))
+    player_start_time = models.DateTimeField(default=now)
+    player_current_index = models.IntegerField(default=0)
+    auction_end_time = models.DateTimeField(default=now)
+    auction_increase_time = models.DurationField(default=timedelta(seconds=10))
+    
     def __str__(self):
         return "Auction Settings"
